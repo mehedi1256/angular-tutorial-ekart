61c6015a99e0af35246fdc706f8e212410f22699
mehedi1256 <mehedihassanshovospirit@gmail.com>
2024-01-08 19:07:46 +0600

    property binding

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,15 @@ import { HeaderComponent } from './header/header.component';
 import { TopHeaderComponent } from './top-header/top-header.component';
 import { TopMenuComponent } from './header/top-menu/top-menu.component';
 import { MainMenuComponent } from './header/main-menu/main-menu.component';
+import { ProductListComponent } from './product-list/product-list.component';
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     TopHeaderComponent,
     TopMenuComponent,
-    MainMenuComponent
+    MainMenuComponent,
+    ProductListComponent
   ],
   imports: [
     BrowserModule
