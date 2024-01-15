4bf9d76887031387f1c8d7c05f96008d431c1650
mehedi1256 <mehedihassanshovospirit@gmail.com>
2024-01-15 19:35:44 +0600

    viewChild

--- src/app/container/container.component.ts
+++ src/app/container/container.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { ProductListComponent } from './product-list/product-list.component';
 
 @Component({
   selector: 'app-container',
@@ -9,6 +10,9 @@ export class ContainerComponent {
   listOfString: string[] = ['Mark', 'Steve', 'Merry', 'John', 'Sarah'];
 
   searchText: string = "";
+
+  @ViewChild(ProductListComponent)
+  productListComponent: ProductListComponent;
 
   setSearchText(value: string) {
     this.searchText = value;
--- src/app/container/product-detail/product-detail.component.ts
+++ src/app/container/product-detail/product-detail.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { Product } from './../../Models/Product';
+import { ProductListComponent } from '../product-list/product-list.component';
 @Component({
   selector: 'product-detail',
   templateUrl: './product-detail.component.html',
   styleUrls: ['./product-detail.component.css']
 })
 export class ProductDetailComponent {
+
+  @Input()
+  productListComp: ProductListComponent = undefined;
+
   product: Product;
+
+  ngOnInit() {
+    this.product = this.productListComp.selectedProduct;
+  }
 }
