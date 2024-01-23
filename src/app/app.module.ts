7b640d7aec2e73abe1a9eee97c40d8fb1e21c7e6
mehedi1256 <mehedihassanshovospirit@gmail.com>
2024-01-23 17:28:48 +0600

    custom attribute  directives

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,7 @@ import { ProductComponent } from './container/product-list/product/product.compo
 import { FilterComponent } from './container/product-list/filter/filter.component';
 import { ProductDetailComponent } from './container/product-detail/product-detail.component';
 import { FeaturedBrandsComponent } from './container/featured-brands/featured-brands.component';
+import { SetBackground } from './CustomDirectives/SetBackground.directive';
 @NgModule({
   declarations: [
     AppComponent,
@@ -28,6 +29,7 @@ import { FeaturedBrandsComponent } from './container/featured-brands/featured-br
     FilterComponent,
     ProductDetailComponent,
     FeaturedBrandsComponent,
+    SetBackground
   ],
   imports: [
     BrowserModule,
